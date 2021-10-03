80b90be5518806383534b99f469e5ce4ded9632d
hana-bananaa <debartosiaki@gmail.com>
2021-10-03 12:13:34 -0300

    aula de parametros em node.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-
 const app = express();
+app.use(express.json());
 // request é oque estamos recebendo da requisição e response é tudo que retorna da requisição
 
 /* Get = Buscar uma informaçao dentro do servidor
@@ -8,20 +8,37 @@ const app = express();
 ** PUT - Alterar uma info no servidor
 ** PATCH - Alterar uma info especifica
 ** DELETE - deletar uma info no servidor
+
+** tipos de parâmetros
+
+1) route params => identificar um recurso editar/deletar/buscar]
+
+2)query params  => paginação, filtros
+
+3) body params => os objetos inserção / alteração (JSON)
 */
 
+
+
     app.get("/courses",(request, response)=>{
+    const query = request.query; 
+    console.log(query);
     return response.json(["curso 1", "curso 2", "curso 3"
     ]);
 });
     // usando metodo post
 
     app.post("/courses",(request, response)=>{
+        const body = request.body;
+        console.log(body);
         return response.json(["curso 1", "curso 2", "curso 3", "curso 4"]);
     //return response.send("hello word!") 
 })
 
     app.put("/courses/:id",(request, response)=>{
+        //testando rout params
+        const {id} = request.params;
+        console.log(id)
         return response.json(["curso 1.1", "curso 2", "curso 3", "curso 4"
         ]);
 });
@@ -29,7 +46,7 @@ const app = express();
 
 
     app.patch("/courses/:id",(request, response)=>{
-        return response.json(["curso 1.0", "curso 2", "curso 3", "curso 4"
+        return response.json(["curso 1.2", "curso 2", "curso 3", "curso 4"
         ]);
     });
         // usando metodo patch
@@ -38,12 +55,6 @@ const app = express();
             return response.json(["curso 1.0", "curso 2", "curso 3"]);
     });
             // usando metodo delete
-        
-        
-
-
-
-
     // usamos normalmente a função de json
    // permite mandar mensagem pra quem solicita a rota
 
