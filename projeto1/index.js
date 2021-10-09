3f38f45193bb9ec3414bce7b3f0e83aee18694b9
hana-bananaa <debartosiaki@gmail.com>
2021-10-09 20:13:28 -0300

    iniciando pagina que mostra cards

--- projeto1/index.js
+++ projeto1/index.js
@@ -1,27 +1,56 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const Pergunta = require ("./database/Pergunta.js")
+// conectando com database
+const connection = require("./database/database.js");
+
+// database
+connection
+    .authenticate()
+    .then(() => {
+        console.log("conexão feita com o banco de dados")
+    })
+    .catch((msgErro)=>{
+        console.log(msgErro)
+    });
+
 // infomando o express usar o ejs como view engine
 app.set('view engine','ejs');
+
+// informando pasta de estaticos
 app.use(express.static('public'));
-app.get("/:nome/:lang",(req, res) => {
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirMsg = false;
 
-    var produto = [
-        {nome:" Doritos", preco:3.14},
-        {nome:"Coca-Cola", preco:2.50},
-        {nome:"Leite",preco:1.45},
-        {nome:"banana", preco:5},
-    ]
+// linkar o body parser com o express
+app.use(bodyParser.urlencoded({ extended : false }))
+app.use(bodyParser.json());
 
-    res.render('index',{
-        nome: nome,
-        lang: lang,
-        empresa : "DEDEBARTO",
-        inscritos: 1,
-        msg : exibirMsg,
-        produtos: produto,
+
+// rotas 
+
+app.get("/",(req, res) => {
+    Pergunta.findAll({ raw:true }).then(perguntas =>{
+        res.render('index',{
+            perguntas : perguntas
+        });
+    });
+   
+});
+
+app.get("/quest",(req, res) => {
+    res.render('quest');
+});
+
+app.post("/takequest",(req, res) => {
+    var title = req.body.title;
+    var description = req.body.description;
+
+    // inserir dados na tabela
+    Pergunta.create({
+        titulo: title,
+        descricao:description
+    }).then(()=>{
+        res.redirect('/');
     });
 });
 
